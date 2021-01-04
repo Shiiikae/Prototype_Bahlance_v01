98b02446bc9c6c730a268e46af9c4fe33945383d
Maëva SENEE <m.senee@etudiant.ecolemelies.fr>
2021-01-05 00:55:43 +0100

    --

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,23 +8,24 @@ public class Menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void PlayGame()
     {
+        Debug.Log("La scene suivante se lance");
         SceneManager.LoadScene("Game_v01");
     }
 
     public void QuitGame()
     {
-        Debug.Log("QUITTE !");
+        Debug.Log("Le jeu est quitter");
         Application.Quit();
     }
 }
